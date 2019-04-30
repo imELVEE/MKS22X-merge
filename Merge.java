7c78a4856db7b16d4fa336e8b1e0280bf7960693
Alvi Aziz <aaziz00@stuy.edu>
2019-04-30 11:24:20 -0400

    debugged

--- Merge.java
+++ Merge.java
@@ -1,6 +1,9 @@
 public class Merge{
   public static void mergesort(int[]data){
-    data = mergesort(data,0,data.length-1);
+    int[] sorted = mergesort(data,0,data.length-1);
+    for (int i = 0 ; i < data.length ; i++){
+      data[i] = sorted[i];
+    }
   }
   private static int[] mergesort(int[]data,int lo,int high){
     if (lo >= high){
@@ -10,10 +13,13 @@ public class Merge{
       }
       return sub;
     }
+    int middle = (high-lo)/2;
     //mergesort left side
-    int[] left = mergesort(data,lo,high-1);
+    int[] left = mergesort(data,lo,lo+middle);
+    //System.out.println(driver.ary(left));
     //mergesort right side
-    int[] right = mergesort(data,lo+1,high);
+    int[] right = mergesort(data,lo+middle+1,high);
+    //System.out.println(driver.ary(right));
     return merge(left,right);
   }
   private static int[] merge(int[] data1, int[] data2){
@@ -22,7 +28,7 @@ public class Merge{
     int counter2 = 0;
     for (int i = 0 ; i < merged.length ; i++){
       if (counter1 < data1.length && counter2 < data2.length){
-        if (data1[counter1] > data2[counter2]){
+        if (data1[counter1] < data2[counter2]){
           merged[i] = data1[counter1];
           counter1++;
         }
