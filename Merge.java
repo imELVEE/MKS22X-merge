8cced681604718047d88e3c115ec1da79ca59558
Alvi Aziz <aaziz00@stuy.edu>
2019-04-30 11:01:44 -0400

    mergesort probably

--- Merge.java
+++ Merge.java
@@ -1,14 +1,20 @@
 public class Merge{
   public static void mergesort(int[]data){
+    data = mergesort(data,0,data.length-1);
   }
-  private static void mergesort(int[]data,int lo,int high){
-    /*
-    if lo >= hi :
-      return
-    mergesort left side
-    mergesort right side
-    merge
-    */
+  private static int[] mergesort(int[]data,int lo,int high){
+    if (lo >= high){
+      int[] sub = new int[high-lo+1];
+      for (int i = 0 ; i < sub.length ; i++){
+        sub[0] = data[i+lo];
+      }
+      return sub;
+    }
+    //mergesort left side
+    int[] left = mergesort(data,lo,high-1);
+    //mergesort right side
+    int[] right = mergesort(data,lo+1,high);
+    return merge(left,right);
   }
   private static int[] merge(int[] data1, int[] data2){
     int[] merged = new int[data1.length + data2.length];
